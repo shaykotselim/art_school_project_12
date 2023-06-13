23286b2d55c2f3ba51de82c1e4b66bffa04e4091
shaykot43500 <hossainshaykot@gmail.com>
2023-06-14 00:44:28 +0600

    added url for instructor

--- src/pages/instructors/Instructors.jsx
+++ src/pages/instructors/Instructors.jsx
@@ -6,7 +6,7 @@ import InstructorCard from "./InstructorCard";
 const Instructors = () => {
     const [instructors, setInstructors] = useState([]);
     useEffect(()=>{
-        fetch("instructors.json")
+        fetch("http://localhost:5000/showinstructor")
         .then(res=>res.json())
         .then(data=> setInstructors(data));
     },[])
